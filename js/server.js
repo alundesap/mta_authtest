23400deeebe8328036efad845373c3f6ef0037d0
alunde <andrew.lunde@sap.com>
2018-02-01 17:45:46 +0000

    better notes

--- js/server.js
+++ js/server.js
@@ -6,7 +6,7 @@ var port = process.env.PORT || 3000;
 
 http.createServer(function (req, res) {
   res.writeHead(200, {"Content-Type": "text/plain"});
-  res.end("This is being served by NodeJS. \n\n Details of the authenticated user can be found here:\n https://shared.authentication.us10.hana.ondemand.com/config?action=who \n");
+  res.end("This is being served by NodeJS. \n\n When deploy to CF shared, details of the authenticated user can be found here:\n https://shared.authentication.us10.hana.ondemand.com/config?action=who \n");
 }).listen(port);
 
 console.log("Server listening on port %d", port);
