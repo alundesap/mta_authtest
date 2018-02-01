4f0c0238901a8cd7588a80fa49353fe37a790161
alunde <andrew.lunde@sap.com>
2018-02-01 17:32:08 +0000

    add files

---
+++ js/server.js
@@ -0,0 +1,12 @@
+/*eslint no-console: 0*/
+"use strict";
+
+var http = require("http");
+var port = process.env.PORT || 3000;
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {"Content-Type": "text/plain"});
+  res.end("This is being served by NodeJS. \n\n Details of the authenticated user can be found here:\n https://shared.authentication.us10.hana.ondemand.com/config?action=who \n");
+}).listen(port);
+
+console.log("Server listening on port %d", port);
